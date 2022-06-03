ceaa2d0c81f897c61e9e404d79880a9271f5d9f4
dembede <onkoba.sammy@gmail.com>
2022-06-03 16:57:56 +0300

    wip: finishing the game, reset button function

--- js/script.js
+++ js/script.js
@@ -6,10 +6,57 @@ const lowOrHi = document.querySelector('.lowOrHi');
 
 const guessSubmit = document.querySelector('.guessSubmit');
 const guessField = document.querySelector('.guessField');
+const gameWrapper = document.querySelector('.game');
 
 let guessCount = 1;
 let resetButton;
 
 function checkGuess() {
-  alert('I am a placeholder');
+  const userGuess = Number(guessField.value);
+  console.log(userGuess);
+  if (guessCount === 1) {
+    guesses.textContent = 'previous guesses: ';
+  }
+  guesses.textContent += userGuess + ' ';
+
+  if (userGuess === randomNumber) {
+    lastResult.textContent = "Bingo! You got it right!!!";
+    lastResult.style.backgroundColor = 'green';
+    lowOrHi.textContent = '';
+    setGameOver();
+  } else if (guessCount === 10) {
+    lastResult.textContent = "!!! GAME OVER !!!";
+    lowOrHi.textContent = '';
+    setGameOver();
+  } else {
+    lastResult.textContent = 'Wrong!';
+    lastResult.style.backgroundColor = 'red';
+    if (userGuess < randomNumber) {
+      lowOrHi.textContent = 'Last guess was too low!';
+    } else if (userGuess > randomNumber) {
+      lowOrHi.textContent = 'Last guess was too high!'
+    }
+  }
+
+  guessCount++;
+  guessField.value = "";
+  guessField.focus();
 }
+
+function setGameOver() {
+  guessField.setAttribute.disabled = true;
+  guessSubmit.setAttribute.disabled = true;
+  resetButton = document.createElement('button');
+  resetButton.textContent = 'Start new game';
+  gameWrapper.append(resetButton);
+  resetButton.addEventListener('click', resetGame);
+}
+function resetGame() {
+  guessCount = 1;
+  const resetParas = document.querySelectorAll('.resultParas p');
+  resetParas.forEach(para => {
+    para.textContent = '';
+  })
+}
+
+guessSubmit.addEventListener('click', checkGuess);
